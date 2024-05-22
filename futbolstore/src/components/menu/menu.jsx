e509c1a2800aad328faec0ac24ae0de0c853bdf5
duban rios <duban0910@gmail.com>
2024-05-22 01:52:43 -0300

    primer guardado

---
+++ futbolstore/src/components/menu/menu.jsx
@@ -0,0 +1,16 @@
+import './menu.css'
+const menu = () => {
+return(
+<div className="container">
+      <h1>FUTBOLSTORE</h1>
+      <div className="menu">
+        <p>INICIO</p>
+        <p>ROPA</p>
+        <p>CONTACTO</p>
+      </div>
+    </div>
+)
+
+}
+
+export default  menu
