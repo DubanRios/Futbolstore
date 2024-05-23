d7eb7d355462945c180db6898adb33d49dfcb5bf
duban rios <duban0910@gmail.com>
2024-05-22 22:56:28 -0300

    tercer guardado

--- futbolstore/src/App.jsx
+++ futbolstore/src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
 import Menu from './components/menu/menu'
+import Cartwidget from './components/Cartwidget/cartwidget'
 
 function App() {
 
 
   return(
     <Menu />
-  )
+
+)
 }
 
 export default App
